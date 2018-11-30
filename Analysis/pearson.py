0afb79351862ee2ee72cdaaaf5d00f4bc9a22398
borea17 <markus.norden@campus.tu-berlin.de>
2018-11-30 09:02:41 +0100

    #added some stuff

--- Analysis/pearson.py
+++ Analysis/pearson.py
@@ -11,12 +11,12 @@ class pearsonAnalysis:
         # print(corr)
         return
 
-    def loadDataFromAPI(self, startTime, endTime):
+    def loadDataFromAPI(self, startTime, endTime, i):
         loc = [49, 8]
         data = dataCollector()
         response = data.getHourlyHistoricData(loc, startTime, endTime)
         self.df = dataCollector.createPandas(response, loc, startTime, endTime)
-        self.saveDataToJSON(self.df, 'pearsonCorrelationLARGE4.json')
+        self.saveDataToJSON(self.df, 'pearsonCorrelationLARGE'+str(i)+'.json')
         return
 
     def saveDataToJSON(self, pandasObject, name):
@@ -26,7 +26,7 @@ class pearsonAnalysis:
 
     def loadAndCreateNewJson(self, nDataSamples=4):
         frames = []
-        for i in range(1,nDataSamples+1):
+        for i in range(0,nDataSamples):
             f = open('pearsonCorrelationLARGE'+str(i)+'.json', 'r')
             string = f.read()
             pand = pd.read_json(string, orient='index')
@@ -39,15 +39,17 @@ PA = pearsonAnalysis()
 startTime = [pd.to_datetime('2017-11-01 00:00'), pd.to_datetime('2017-11-10 00:00'),
              pd.to_datetime('2017-11-19 00:00'), pd.to_datetime('2017-11-28 00:00'),
              pd.to_datetime('2017-12-03 00:00'), pd.to_datetime('2017-12-12 00:00'),
-             pd.to_datetime('2017-12-21 00:00')]
+             pd.to_datetime('2017-12-21 00:00'), pd.to_datetime('2017-12-31 00:00'),
+             pd.to_datetime('2018-01-08 00:00'), pd.to_datetime('2018-01-18 00:00')]
 endTime = [pd.to_datetime('2017-11-10 00:00'), pd.to_datetime('2017-11-19 00:00'),
            pd.to_datetime('2017-11-28 00:00'), pd.to_datetime('2017-12-03 00:00'),
            pd.to_datetime('2017-12-12 00:00'), pd.to_datetime('2017-12-21 00:00'),
-           pd.to_datetime('2017-12-31 00:00'),]
+           pd.to_datetime('2017-12-31 00:00'), pd.to_datetime('2018-01-08 00:00'),
+           pd.to_datetime('2018-01-18 00:00'), pd.to_datetime('2018-01-28 00:00')]
 lag = 30
 for i in range(len(startTime)):
-    PA.loadDataFromAPI(startTime[i], endTime[i])
-result = PA.loadAndCreateNewJson()
+    PA.loadDataFromAPI(startTime[i], endTime[i], i)
+result = PA.loadAndCreateNewJson(len(startTime))
 MLobject = ML(result)
 newPD, corr = MLobject.applyAnalysis(lag)
 f = open('pCorr.csv', 'w')
